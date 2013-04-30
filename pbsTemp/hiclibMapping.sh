38153b28eb0219731c4f8f1e94f9b12454787f6d
Fabian Buske <f.buske@garvan.org.au>
2013-04-30 21:04:31 +1000

    bugfix hiclib mapping script

--- pbsTemp/hiclibMapping.sh
+++ pbsTemp/hiclibMapping.sh
@@ -147,7 +147,7 @@ if [ -n "$HICLIB_READLENGTH" ]; then
 	PARAMS="$PARAMS --readLength $HICLIB_READLENGTH"
 fi
 
-python $HISEQINF/bin/hiclib.py ${PARAMS} --bowtie $(which bowtie2) --cpus $THREADS --outputDir $MYOUT --tmpDir $TMP --verbose $READS
+python $HISEQINF/bin/hiclibMapping.py ${PARAMS} --bowtie $(which bowtie2) --cpus $THREADS --outputDir $MYOUT --tmpDir $TMP --verbose $READS
 
 echo "********* merge bam files"
 
