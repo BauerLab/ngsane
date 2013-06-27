2a8e26266be7d5ae4b93f7f37f5af68811978e30
Fabian Buske <f.buske@garvan.org.au>
2013-06-27 11:10:04 +1000

    Default thread parameters adjusted

--- mods/trimmomatic.sh
+++ mods/trimmomatic.sh
@@ -42,9 +42,9 @@ echo "PATH=$PATH"
 #this is to get the full path (modules should work but for path we need the full path and this is the\
 # best common denominator)
 PATH_TRIMMOMATIC=$(dirname $(which trimmomatic.jar))
-echo -e "--JAVA        --\n" $(java $JAVAPARAMS -version 2>&1)a
+echo -e "--JAVA        --\n" $(java $JAVAPARAMS -version 2>&1)
 [ -z "$(which java)" ] && echo "[ERROR] no java detected" && exit 1
-echo -e "--trimmomatic --\n " $(which $PATH_TRIMMOMATIC/trimmomatic.jar 2>&1)
+echo -e "--trimmomatic --\n " $(which $PATH_TRIMMOMATIC/trimmomatic.jar)
 [ ! -f $PATH_TRIMMOMATIC/trimmomatic.jar ] && echo "[ERROR] no trimmomatic detected" && exit 1
 
 # check if trimming steps are set
