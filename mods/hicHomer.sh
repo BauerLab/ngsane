12d80c1a94c01d98210e34012c77ba2810c3b578
Fabian Buske <f.buske@garvan.org.au>
2013-07-29 09:58:51 +1000

    Pair library files required to be different to qualify for paired runs

--- mods/hicHomer.sh
+++ mods/hicHomer.sh
@@ -65,7 +65,7 @@ echo -e "--circos  --\n "$(circos --version)
 n=${f##*/}
 
 #is paired ?                                                                                                      
-if [ -n "$READTWO" ] && [ -e ${f/$READONE/$READTWO} ]; then
+if [ "$f" != "${f/$READONE/$READTWO}" ] && [ -e ${f/$READONE/$READTWO} ]; then
     PAIRED="1"
 else
     PAIRED="0"
