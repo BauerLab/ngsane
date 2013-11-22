78bbbb33c7108525c9d94f88e1bf396d6a8e3b62
Fabian Buske <f.buske@garvan.org.au>
2013-11-22 23:13:11 +1100

    Phred encoding options simplified

--- mods/bwa.sh
+++ mods/bwa.sh
@@ -148,9 +148,9 @@ fi
 if [ -z "FASTQ_ENCODING" ]; then 
     FASTQ_ENCODING=$($ZCAT $f |  awk 'NR % 4 ==0' | python $NGSANE_BASE/tools/GuessFastqEncoding.py |  tail -n 1)
 fi
-if [[ "$FASTQ_ENCODING" == *Phred33* || "$FASTQ_ENCODING" == "Sanger" ]]; then
+if [[ "$FASTQ_ENCODING" == "Phred33" ]]; then
     FASTQ_PHRED=""    
-elif [[ "$FASTQ_ENCODING" == *Illumina* || "$FASTQ_ENCODING" == *Solexa* || "$FASTQ_ENCODING" == "Phred64" ]]; then
+elif [[ "$FASTQ_ENCODING" == "Phred64" ]]; then
     FASTQ_PHRED="-I"
 else
     echo "[NOTE] cannot detect/don't understand fastq format: $FASTQ_ENCODING - using default"
