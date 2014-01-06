15ad7fd1fbc76e09e2c8dd885ba2a75abc34e7cd
Fabian Buske <fabian.buske@gmail.com>
2014-01-06 12:39:26 +1100

    fit-hi-c additions

--- conf/sample_header.sh
+++ conf/sample_header.sh
@@ -65,7 +65,7 @@ NG_HTSEQ=${NG_PYTHON}
 NG_CIRCOS=
 NG_BLAT=
 NG_PINDEL=
-
+NG_SEQLOGO=
 
 ##############################################################
 # Software reference
@@ -108,6 +108,7 @@ NG_CITE_HTSEQ="Conf Proc IEEE Eng Med Biol Soc. 2013 Jul;2013:647-50. doi: 10.11
 NG_CITE_CIRCOS="Genome Res. 2009 Sep;19(9):1639-45. doi: 10.1101/gr.092759.109. Epub 2009 Jun 18. 'Circos: an information aesthetic for comparative genomics.'; Krzywinski M, Schein J, Birol I, Connors J, Gascoyne R, Horsman D, Jones SJ, Marra MA."
 NG_CITE_BLAT="Genome Res. 2002 Apr;12(4):656-64. 'BLAT--the BLAST-like alignment tool.'; Kent WJ."
 NG_CITE_PINDEL="Ye K, Schulz MH, Long Q, Apweiler R, Ning Z. Pindel: a pattern growth approach to detect break points of large deletions and medium sized insertions from paired-end short reads. Bioinformatics. 2009 Nov 1;25(21):2865-71. Epub 2009 Jun 26."
+NG_CITE_SEQLOGO="Nucleic Acids Res. 1990 Oct 25;18(20):6097-100.; 'Sequence logos: a new way to display consensus sequences.'; Schneider TD, Stephens RM."
 
 ##############################################################
 # PROGRAM PATHS
