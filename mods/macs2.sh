ce36e01f5087dd1d243499cad68601ed12c3618e
Fabian Buske <fabian.buske@gmail.com>
2014-01-08 15:30:34 +1100

    checkpointing revised

--- mods/macs2.sh
+++ mods/macs2.sh
@@ -153,6 +153,8 @@ else
     if [ "$(grep '#2 predicted fragment length is -' $SAMPLE.summary.txt)" > 0 ]; then
         echo "[NOTE] Sample has negative fragment length, adjusting shiftsize with alternatives if available"
 
+        [ -f $SAMPLE"_"peaks.xls ] && rm $SAMPLE"_"peaks.xls
+    
         if [ "$(grep '#2 alternative fragment length(s) may be' $SAMPLE.summary.txt | egrep -o '(-[0-9]{2,4},[0-9]{2,4})' | cut -d ',' -f 2)" > 0 ]; then
             SHIFTSIZE="--shiftsize $(grep '#2 alternative fragment length(s) may be' $SAMPLE.summary.txt | egrep -o '(-[0-9]{2,4},[0-9]{2,4})' | cut -d ',' -f 2)"           
         fi
