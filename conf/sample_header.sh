5072ad324920259bf972c177ec67b3e25a597cc9
Fabian Buske <fabian.buske@gmail.com>
2014-01-29 16:18:15 +1100

    citation updated

--- conf/sample_header.sh
+++ conf/sample_header.sh
@@ -71,7 +71,7 @@ NG_MASAI=
 ##############################################################
 # Software reference
 ##############################################################
-NG_CITE_NGSANE="(accepted); 'NGSANE: A Lightweight Production Informatics Framework for High Throughput Data Analysis; Buske FA, French HJ, Smith MA, Clark SJ, Bauer DC"
+NG_CITE_NGSANE="Bioinformatics 2014; doi: 10.1093/bioinformatics/btu036 'NGSANE: A Lightweight Production Informatics Framework for High Throughput Data Analysis; Buske FA, French HJ, Smith MA, Clark SJ, Bauer DC"
 NG_CITE_R="R: A language and environment for statistical computing; R Core Team. R Foundation for Statistical Computing, Vienna, Austria, (2013)"
 NG_CITE_PYTHON="Guido van Rossum, Jelke de Boer: Linking a Stub Generator (AIL) to a Prototyping Language (Python). Spring 1991 EurOpen Conference Proceedings (May 20-24, 1991) Tromso, Norway."
 NG_CITE_FASTQC="http://www.bioinformatics.babraham.ac.uk/projects/fastqc/"
