7c8fb51cc35b0aeda40363665818de511107a194
Fabian Buske <fabian.buske@gmail.com>
2014-03-17 17:30:17 +1100

    estimate fragment size automatically using macs2

--- mods/wiggler.sh
+++ mods/wiggler.sh
@@ -57,8 +57,14 @@ echo "PATH=$PATH"
 # best common denominator)
 
 echo -e "--NGSANE      --\n" $(trigger.sh -v 2>&1)
+echo -e "--macs2       --\n "$(macs2 --version 2>&1)
+[ -z "$(which macs2)" ] && echo "[ERROR] macs2 not detected" && exit 1
 echo -e "--wiggler     --\n "$(align2rawsignal 2>&1 | head -n 3 | tail -n 1)
 [ -z "$(which align2rawsignal)" ] && echo "[ERROR] wiggler not detected (align2rawsignal)" && exit 1
+echo -e "--wigToBigWig --\n "$(wigToBigWig 2>&1 | tee | head -n 1)
+[ -z "$(which wigToBigWig)" ] && echo "[WARN] wigToBigWig not detected, cannot create bigwigs"
+echo -e "--bedToBigBed --\n "$(bedToBigBed 2>&1 | tee | head -n 1 )
+[ -z "$(which bedToBigBed)" ] && echo "[WARN] bedToBigBed not detected, cannot create bedgraphs"
 
 echo -e "\n********* $CHECKPOINT\n"
 ################################################################################
@@ -72,6 +78,13 @@ SAMPLE=${n/%.$ASD.bam/}
 if [ -z "$WIGGLER_UMAPDIR" ] || [ ! -d ${WIGGLER_UMAPDIR} ]; then
     echo "[ERROR] umap dir not specified not non-existant (WIGGLER_UMAPDIR)"
     exit 1
+fi
+
+GENOME_CHROMSIZES=${FASTA%.*}.chrom.sizes
+if [ ! -f $GENOME_CHROMSIZES ]; then
+    echo "[WARN] GENOME_CHROMSIZES not found. Expected at $GENOME_CHROMSIZES"
+else
+    echo "[NOTE] Chromosome size: $GENOME_CHROMSIZES"
 fi
 
 # check reference folder exist
@@ -101,19 +114,46 @@ fi
 
 echo -e "\n********* $CHECKPOINT\n"
 ################################################################################
+CHECKPOINT="calculate fragment size"
+
+if [[ -n "$RECOVERFROM" ]] && [[ $(grep -P "^\*{9} $CHECKPOINT" $RECOVERFROM | wc -l ) -gt 0 ]] ; then
+    echo "::::::::: passed $CHECKPOINT"
+else 
+    
+    if [ -z "$WIGGLER_FRAGMENTSIZE" ]; then
+        macs2 predictd -i $f > $OUTDIR/$SAMPLE.fragment_size.txt 2>&1
+    fi
+    
+    # mark checkpoint
+    if [[ $WIGGLER_FRAGMENTSIZE =~ '^[0-9]+$' ]] || [ -f $OUTDIR/$SAMPLE.fragment_size.txt ];then echo -e "\n********* $CHECKPOINT\n"; unset RECOVERFROM; else echo "[ERROR] checkpoint failed: $CHECKPOINT"; exit 1; fi
+    
+fi 
+################################################################################
 CHECKPOINT="run align2rawsignal"
 
 if [[ -n "$RECOVERFROM" ]] && [[ $(grep -P "^\*{9} $CHECKPOINT" $RECOVERFROM | wc -l ) -gt 0 ]] ; then
     echo "::::::::: passed $CHECKPOINT"
 else 
     
-    RUN_COMMAND="align2rawsignal $WIGGLERADDPARAMS -of=$WIGGLER_OUTPUTFORMAT -i=$f -s=${FASTA_CHROMDIR} -u=${WIGGLER_UMAPDIR} -v=$OUTDIR/$SAMPLE.log -o=$THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT -mm=$MEMORY_WIGGLER"
+    if [ -z "$WIGGLER_FRAGMENTSIZE" ]; then
+        WIGGLER_FRAGMENTSIZE=$(grep 'alternative fragment length(s) may be' $OUTDIR/$SAMPLE.fragment_size.txt | sed 's/.* be //' | egrep -o '((-[0-9]{2,4},)?[0-9]{2,4})')
+    fi
+    
+    [ -f $OUTDIR/$SAMPLE.log ] && rm $OUTDIR/$SAMPLE.log
+    
+    RUN_COMMAND="align2rawsignal $WIGGLERADDPARAMS -f=$WIGGLER_FRAGMENTSIZE -of=$WIGGLER_OUTPUTFORMAT -i=$f -s=${FASTA_CHROMDIR} -u=${WIGGLER_UMAPDIR} -v=$OUTDIR/$SAMPLE.log -o=$THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT -mm=$MEMORY_WIGGLER"
     echo $RUN_COMMAND && eval $RUN_COMMAND
 
-    $GZIP -c $THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT > $OUTDIR/$SAMPLE.$WIGGLER_OUTPUTFORMAT.gz
+    if [[ "$WIGGLER_OUTPUTFORMAT" == "bg" ]] && [[ hash bedToBigBed ]] && [[ -f $GENOME_CHROMSIZES ]]; then
+        bedGraphToBigWig $THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT $GENOME_CHROMSIZES $OUTDIR/$SAMPLE.bw
+    elif [[ "$WIGGLER_OUTPUTFORMAT" == "wig" ]] && [[ hash wigToBigWig ]] && [[ -f $GENOME_CHROMSIZES ]]; then 
+        wigToBigWig $THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT $GENOME_CHROMSIZES $OUTDIR/$SAMPLE.bw
+    else
+        $GZIP -c $THISTMP/$SAMPLE.$WIGGLER_OUTPUTFORMAT > $OUTDIR/$SAMPLE.$WIGGLER_OUTPUTFORMAT.gz  
+    fi
         
     # mark checkpoint
-    if [ -f $OUTDIR/$SAMPLE.$WIGGLER_OUTPUTFORMAT.gz ];then echo -e "\n********* $CHECKPOINT\n"; unset RECOVERFROM; else echo "[ERROR] checkpoint failed: $CHECKPOINT"; exit 1; fi
+    if [ -f $OUTDIR/$SAMPLE.$WIGGLER_OUTPUTFORMAT.gz ] || [ -f $OUTDIR/$SAMPLE.bw ];then echo -e "\n********* $CHECKPOINT\n"; unset RECOVERFROM; else echo "[ERROR] checkpoint failed: $CHECKPOINT"; exit 1; fi
     
 fi 
 ################################################################################
