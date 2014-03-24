3ec8975cb2731dd9665b2ad32d3aeb358d8c229d
Fabian Buske <fabian.buske@gmail.com>
2014-03-24 14:09:45 +1100

    p-value calculation added

--- mods/fseq.sh
+++ mods/fseq.sh
@@ -61,6 +61,8 @@ echo -e "--wigToBigWig --\n "$(wigToBigWig 2>&1 | tee | head -n 1)
 [ -z "$(which wigToBigWig)" ] && echo "[ERROR] wigToBigWig not detected" && exit 1
 echo -e "--fseq        --\n "$(fseq -v | head -n 1)
 [ -z "$(which fseq)" ] && echo "[ERROR] no fseq detected" && exit 1
+echo -e "--R           --\n "$(R --version | head -n 3)
+[ -z "$(which R)" ] && echo "[ERROR] no R detected" && exit 1
 
 echo -e "\n********* $CHECKPOINT\n"
 ################################################################################
@@ -89,6 +91,10 @@ if [ ! -f $GENOME_CHROMSIZES ]; then
     exit 1
 else
     echo "[NOTE] Chromosome size: $GENOME_CHROMSIZES"
+fi
+
+if [ -z "$FSEQ_PVALUECUTOFF" ] || [ $FSEQ_PVALUECUTOFF -gt 1 ] ; then
+    FSEQ_PVALUECUTOFF=0.05
 fi
 
 # unique temp folder that should be used to store temporary files
@@ -137,6 +143,32 @@ else
 
     RUN_COMMAND="cat $THISTMP/*.npf > $OUTDIR/$SAMPLE.narrowPeak"
     echo $RUN_COMMAND && eval $RUN_COMMAND
+
+    # mark checkpoint
+    if [ -f $OUTDIR/$SAMPLE.narrowPeak ];then echo -e "\n********* $CHECKPOINT\n"; unset RECOVERFROM; else echo "[ERROR] checkpoint failed: $CHECKPOINT"; exit 1; fi
+fi
+
+################################################################################
+CHECKPOINT="estimate p-values"
+
+if [[ -n "$RECOVERFROM" ]] && [[ $(grep -P "^\*{9} $CHECKPOINT" $RECOVERFROM | wc -l ) -gt 0 ]] ; then
+    echo "::::::::: passed $CHECKPOINT"
+else
+
+    # estimate p-value by fitting gamma distribution
+    cat > $OUTDIR/$SAMPLE.R <<DELIM    
+library(MASS)
+x <- read.delim("$OUTDIR/$SAMPLE.narrowPeak", header=F)
+g<-MASS::fitdistr(x$V7,"gamma")
+x$V8<-sapply(x$V7, function(y) ks.test(y, "pgamma",shape=g$estimate["shape"],rate=g$estimate["rate"])$p.value)
+write.table(x, "$OUTDIR/$SAMPLE.narrowPeak.tmp", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
+DELIM
+    
+    Rscript --vanilla $OUTDIR/$SAMPLE.R
+
+    #filter with p-value cutoff
+    awk -v CUTOFF=$FSEQ_PVALUECUTOFF '{if ($8 <= FSEQ_CUTOFF}{print $0}' $OUTDIR/$SAMPLE.narrowPeak.tmp > $OUTDIR/$SAMPLE.narrowPeak
+    rm $OUTDIR/$SAMPLE.narrowPeak.tmp
 
     # mark checkpoint
     if [ -f $OUTDIR/$SAMPLE.narrowPeak ];then echo -e "\n********* $CHECKPOINT\n"; unset RECOVERFROM; else echo "[ERROR] checkpoint failed: $CHECKPOINT"; exit 1; fi
