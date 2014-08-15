3b5b206dea63129a5fa1e07408a306fe3663e298
Fabian Buske <f.buske@garvan.org.au>
2014-08-15 20:29:55 +1000

    Bugfix number formatting

--- core/includes/js/ngsane.js
+++ core/includes/js/ngsane.js
@@ -3,15 +3,24 @@ function getHash( url ) {
     return url.substring( hashPos + 1 );
 }
 
-function isNumber(obj) { 
-    return !isNaN(parseFloat(obj));
-}
-
 // format number with comma
 function formatNumber(n) {
-    if (isNumber(n))
-        return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    else
+    // only attempt conversion if n is numberic
+    if (!isNaN(parseFloat(n))){
+        var t = Math.abs(parseFloat(n));
+        console.log(t);
+        if (t == 0)
+            return n;
+        else if (t < 0.01)
+            return t.toExponential();
+        else
+            // treat pre and post differently
+            var n= t.toString().split(".");
+            // add comma to decimal positions only
+            n[0] = n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            //Combine both sections again
+            return n.join(".");
+    } else
         return n;
 }
 
