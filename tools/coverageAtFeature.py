Bugfix two-sided feature coverage calculation

tools/coverageAtFeature.py
@@ -27,24 +27,21 @@ import argparse, sys, numpy, math, traceback
 #    matrix = numpy.delete(matrix, (0), axis=0)
 #    return (numpy.mean(matrix, axis=0), numpy.std(matrix, axis=0))
                     
-
-def processStream2(file, up, center, down, bin, length, args):
-    matrix=numpy.empty(shape=(length,up+center+down))
-    counter=0
+def processStream3(matrix, file, offset, bins, bin, length, args):
+    counter =0
     for f in open(file):
         try:
-            #arr=map(float,f.split("\t"))
-            arr=f.split("\t")
-            #print "%i %s %s %i %s" % (counter/(up+down), arr[0], arr[1], len(arr),str(arr))
-    #        if (len(arr)>2 and arr[2]=="-"):
-    #       matrix[counter/(up+down),((up+down)-int(arr[0]))/bin]=float(arr[1])   
-    #        else:
-            matrix[counter/(up+center+down),(int(arr[0])-1)/bin]=float(arr[1])
+            arr=f.strip().split("\t")
+            matrix[counter/(bins),offset+(int(arr[0])-1)/bin]=float(arr[1])
             counter+=1
         except:
             print arr
             print traceback.format_exc()
-        
+            print "%d %d %d %d %d" % (offset, bins, bin, counter, counter/bins)
+            sys.exit(1)
+#    print "%d %d %d %d %d" % (offset, bins, bin, counter, counter/bins)
+
+def calculateMetrics(matrix,args):
     print "shape of matrix %s" % (str(matrix.shape))
 
     #ignore all genomic locations that have zero coverage
@@ -80,37 +77,91 @@ def processStream2(file, up, center, down, bin, length, args):
     #calculate median
     if (args.metric=="median"):
         print "median"
-        return (numpy.median(matrix, axis=0), numpy.std(matrix, axis=0)*(1/math.sqrt(length)))
+        return (numpy.median(matrix, axis=0), numpy.std(matrix, axis=0)*(1/math.sqrt(args.length)))
     if (args.metric=="sum"):
         print "sum"
         return (numpy.sum(matrix, axis=0), None)
     print "mean"
+    return (numpy.mean(matrix, axis=0), numpy.std(matrix, axis=0)*(1/math.sqrt(args.length)))
+
+def processStream2(file, up, center, down, bin, length, args):
+    matrix=numpy.empty(shape=(length,up+center+down))
+    counterUp=0
+    counterCenter=0 
+    counterDown=0
+    counter =0
+    for f in open(file):
+        try:
+            #arr=map(float,f.split("\t"))
+            arr=f.strip().split("\t")
+            #print "%i %s %s %i %s" % (counter/(up+down), arr[0], arr[1], len(arr),str(arr))
+    #        if (len(arr)>2 and arr[2]=="-"):
+    #       matrix[counter/(up+down),((up+down)-int(arr[0]))/bin]=float(arr[1])   
+    #        else:
+            
+            if (int(arr[0]) <= args.upstream):
+                matrix[counterUp/(up),(int(arr[0])-1)/bin]=float(arr[1])
+                counterUp+=1
+            elif (int(arr[0]) <= args.upstream+args.center):
+                matrix[counterCenter/(center),up/bin+(int(arr[0])-1)]=float(arr[1])
+                countercenter +=1
+            elif (int(arr[0]) <= args.upstream+args.center+args.downstream):
+                matrix[counterDown/(down),up/bin+center+(int(arr[0])-1)/bin]=float(arr[1])
+                counterDown +=1
+            else:
+                print "something odd happend"
+                print arr
+                print "%d,%d,%d,%d" % (args.upstream,args.center,args.downstream, counter)
+                sys.exit(1)
+#                
+#
+            counter+=1
+        except:
+            print arr
+            print traceback.format_exc()
+            sys.exit(1)
+        
+    print "shape of matrix %s" % (str(matrix.shape))
+
+    #ignore all genomic locations that have zero coverage
+    if (args.ignore):
+        z = numpy.all(matrix==0, axis=1)
+        matrix = matrix.compress(numpy.logical_not(z), axis=0)
+        print "shape of matrix %s after removal of zero coverage" % (str(matrix.shape))
+    if (matrix.shape[0]==0):
+        print("[ERROR] all TTS were removed for this mark, consider a less stringent filtering")
+        sys.exit()
+
+    #normalize by total reads in library
+    if (args.normalize):
+        # add pseudocount just in case
+        if (args.normalize == 0):
+            args.normalize = 0.001
+        matrix=matrix*(1.0/args.normalize)
+        print "normalize by %i" % (args.normalize)
+
+
+    #remove outliers
+    if (args.timestd):
+        #std=numpy.std(matrix)
+        #z = numpy.any(matrix>(std*args.TIMESSTD), axis=1)
+        std=numpy.std(matrix,axis=0)
+        mask=numpy.zeros(shape=(len(matrix)))
+        for r in range(0,numpy.shape(matrix)[1]):
+            loc=numpy.where(matrix[:,r]>(std[r]*args.timestd))
+            mask[loc]=1
+        matrix = matrix.compress(numpy.logical_not(mask), axis=0)
+        print "shape of matrix %s after outlier removal" % (str(matrix.shape))
+
+    #calculate median
+    if (args.metric=="median"):
+#        print "median"
+        return (numpy.median(matrix, axis=0), numpy.std(matrix, axis=0)*(1/math.sqrt(length)))
+    if (args.metric=="sum"):
+#        print "sum"
+        return (numpy.sum(matrix, axis=0), None)
+#    print "mean"
     return (numpy.mean(matrix, axis=0), numpy.std(matrix, axis=0)*(1/math.sqrt(length)))
-
-
-#def processNumpy(file,up,down,length):
-#    x=numpy.loadtxt(open(file,"rb"),delimiter="\t")
-#    ndx = np.lexsort(keys=(data, id))
-#    id, data = id[ndx], data[ndx]
-#    df = DataFrame(dict(id=id, data=data))
-#    print x 
-
-
-#def plot():
-#    print len(std)
-#    print len(x)
-#    df= pd.DataFrame({'mean':mean,'std':std,'x':x})
-#    h = com.convert_to_r_dataframe(df)
-#
-#    grdevices = importr('grDevices')
-#    print "plot "+args.image
-#    grdevices.png(file=args.image, width=1300, height=1000)
-#
-#    p =ggplot2.ggplot(h) + \
-#       ggplot2.aes_string(x = "x") + \
-#       ggplot2.geom_line(ggplot2.aes_string(y="mean"),color='red') + \
-#       ggplot2.geom_ribbon(ggplot2.aes_string(ymin="mean-std", ymax="mean+std"))
-#    p.plot()
 
 
 def writeR(metric,ste,xvalue,file,name,category,args):
@@ -141,6 +192,9 @@ def writeCode(file, label, args):
         """
         library("ggplot2")
         library("plyr")
+        upstream=%d
+        center=%d
+        downstream=%d
         result <- read.table('%s', header=T)
         result2<-ddply(.data=result, .(x,mark,category), summarize, metric=mean(metric)%s)
         pdf('%s.pdf', width=10, height=5*length(levels(result$category)))
@@ -148,12 +202,12 @@ def writeCode(file, label, args):
             geom_line(aes(y=metric, col=mark)) +
             labs(title = "Coverage Distribution", y = "%s coverage", x = "") +
             geom_vline(xintercept = 0, colour = "gray65") +
-            geom_vline(xintercept = %d, colour = "gray65") +
+            geom_vline(xintercept = center, colour = "gray65") +
             facet_grid(category ~ ., scales = "free_y") +
-            annotate("text", x = %d, y = 0, label = "%s") + 
-            scale_x_continuous(breaks=c(seq(%d, 0, 200),seq(%d,%d,200)), labels=c(seq(%d, 0, 200),seq(0,%d,200)))
+            annotate("text", x = center/2, y = 0, label = "%s") + 
+            scale_x_continuous(breaks=c(seq(-upstream, 0, upstream/5),seq(center,center+downstream,downstream/ 5)), labels=c(seq(-upstream, 0, upstream/5),seq(0,downstream,downstream/5)))
         dev.off()
-        """ % (file+".txt",ddply,file,geomribbon,args.metric, args.center,args.center/2, label,-args.upstream,args.center,args.center+args.downstream,-args.upstream,args.downstream))
+        """ % (args.upstream,args.center,args.downstream, file+".txt",ddply,file,geomribbon,args.metric, label))
     else:
         code.write(
         """
@@ -196,14 +250,30 @@ def main(args):
 
     upstream=args.upstream/args.bin
     downstream=(args.downstream+1)/args.bin
-
+    center=(args.center+1)
     #print "%i %i" % (upstream, downstream)
 
     #processNumpy(args.inputfile,args.upstream,args.downstream,args.length)
     #mean,std=processStream(args.inputfile,args.upstream,args.downstream,args.length)
     if (args.inputfile):
-        metric,ste=processStream2(args.inputfile,upstream,args.center,downstream,args.bin,args.length, args)
-        x=range(-(args.upstream),args.center+args.downstream,args.bin)
+        matrix=numpy.empty(shape=(args.length,upstream+center+downstream))
+        offset=0
+        if (args.upstream>0):
+#            print "upstream"
+            processStream3(matrix,args.inputfile+"_upstream",offset,upstream,args.bin,args.length, args)
+            offset+=upstream
+        if (args.center>0):
+#            print "center"
+            processStream3(matrix,args.inputfile+"_center",offset,center,1,args.length, args)
+            offset+=args.center
+        if (args.downstream>0):
+#            print "downstream"
+            processStream3(matrix,args.inputfile+"_downstream",offset,downstream,args.bin,args.length, args)
+            
+        metric,ste=calculateMetrics(matrix, args)
+        
+        x=range(-args.upstream,0,args.bin)+range(0,center,1)+range(center,center+args.downstream,args.bin)
+
         writeR(metric,ste,x,args.rfile,args.name,args.category,args)
     if (args.image) :
         writeCode(args.rfile,args.genomic,args)